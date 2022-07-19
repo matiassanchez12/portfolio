9dca51808ee55913d5640c032c490c6a0c9a5437
Matias <matias.sanchez.1897@hotmail.com>
2022-07-19 14:50:04 -0300

    update last projects

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -1,17 +1,24 @@
 export const projects = [
   {
+    name: "Space page",
+    urlRepo: "https://github.com/matiassanchez12/space",
+    urlDeploy: "https://space-lovat.vercel.app/",
+    img: "/space.png",
+    technologies: ["React", "Chakra UI", "Typescript", "Challenge"],
+  },
+  {
+    name: "Staem",
+    urlRepo: "https://github.com/matiassanchez12/staem",
+    urlDeploy: "https://staem-ten.vercel.app/",
+    img: "/staem.png",
+    technologies: ["React", "Next.js", "Chakra UI", "Typescript", "Challenge"],
+  },
+  {
     name: "Calendar Todo App",
     urlRepo: "https://github.com/matiassanchez12/todoapp",
     urlDeploy: "https://calendartodo.vercel.app/",
     img: "/calendar.png",
     technologies: ["React", "Next", "Chakra UI"],
-  },
-  {
-    name: "Mis gastos App",
-    urlRepo: "https://github.com/matiassanchez12/challenge-alkemy",
-    urlDeploy: "https://challenge-front-rust.vercel.app/",
-    img: "/gastos.png",
-    technologies: ["React", "Next", "Chakra UI", "Challenge"],
   },
   {
     name: "Portfolio Personal",
@@ -21,24 +28,18 @@ export const projects = [
     technologies: ["React", "Next", "Chakra UI", "Framer Motion"],
   },
   {
-    name: "Easy Bank",
-    urlRepo: "https://github.com/matiassanchez12/Easybank-page-",
-    urlDeploy: "https://easybank-page-three.vercel.app/",
-    img: "/easybank.png",
-    technologies: ["React", "Firebase", "Styled components", "Challenge"],
+    name: "Mis gastos App",
+    urlRepo: "https://github.com/matiassanchez12/challenge-alkemy",
+    urlDeploy: "https://challenge-front-rust.vercel.app/",
+    img: "/gastos.png",
+    technologies: ["React", "Chakra UI", "Challenge"],
   },
+
   {
     name: "Where in the world?",
     urlRepo: "https://github.com/matiassanchez12/Flags-page",
     urlDeploy: "https://flagspage.vercel.app/",
     img: "/flagpage.png",
     technologies: ["React", "Styled components", "Challenge"],
-  },
-  {
-    name: "Punto de venta",
-    urlRepo: "https://github.com/matiassanchez12/pos",
-    urlDeploy: "https://boiling-peak-24210.herokuapp.com/",
-    img: "/pointofsale.png",
-    technologies: ["Codeigniter 4", "Bootstrap 4", "MySql"],
   },
 ];
